42b227072506cf16af50290cea96957a47bd90bc
adosib <adosib1@gmail.com>
2020-06-06 21:06:03 -0500

    refactored and improved get_channel_details to minimize the number of API calls made; added helper functions

--- collector.py
+++ collector.py
@@ -5,7 +5,54 @@ from dateutil.relativedelta import relativedelta
 from googleapiclient.discovery import build
 
 
-# TODO: refactor into a class YTVideoSearch
+def check_channel_id(channels_list: list, path: str) -> list:
+    """
+    Checks if there exists data for the provided channel ids in the 
+    directory (path arg), assuming the directory contains files of format
+    <channel_id>.<ext>
+    Args:
+    - channel_ids: a list of channel ids
+    - path: path to the directory to check
+
+    Returns a filtered list of channel_ids that were not found in the directory.
+    """
+    channels_in_dir = os.listdir(path)
+    # remove the file extension for the files in the dir
+    channels_in_dir = [
+        os.path.splitext(channel)[0] for channel in channels_in_dir
+    ]
+
+    to_remove = []
+    # do a lookup of the channel id in the /data/channels directory to see if a file already exists
+    for channel_id in channels_list:
+        if channel_id in channels_in_dir:
+            to_remove.append(channel_id)
+
+    # remove the channel id from the list
+    [channels_list.remove(channel_id) for channel_id in to_remove]
+
+    return channels_list
+
+
+def get_response_item(resource_property: str, key: str, channel_id: str, path: str):
+    """
+    Get the value for the key in the resource_property dict for the provided 
+    channel_id and path.
+    Args:
+    - resource_property: a key in the response dict corresponding to the resource property ('part') to search
+    - key: key to search for in the resource_property dict
+    - channel_id: channel id
+    - path: directory to search; must end with trailing /
+    """
+    ext = '.json'
+    with open(path+channel_id+ext) as response:
+        data = json.load(response)
+
+    item = data[resource_property][key]
+
+    return item
+
+
 def page_through_response(service_instance, request, response) -> dict:
     """
     Pages through a response by calling on the list_next method of the service instance.
@@ -31,43 +78,25 @@ def page_through_response(service_instance, request, response) -> dict:
 
 def get_channel_details(service, channel_ids, part='snippet,statistics,brandingSettings', results=50):
     """
-    Makes use of the channel resource list method. 
+    Makes use of the channel resource list method. Gets channel data for 
+    resource properties defined in the part argument and writes the repsonse
+    to JSON files in data/channels
+
     Args:
     - service: The YouTube Data API service
-    - channel_ids: A comma-separated list of the YouTube channel ID(s)
+    - channel_ids: A comma-separated string of the YouTube channel ID(s)
     - part: A comma-separated string of channel resource properties
     - results: Max results returned in the reponse
-
-    Returns the following data:
-    - snippet.publishedAt: The date and time that the channel was created
-    - statistics.viewCount: The number of times the channel has been viewed
-    - statistics.commentCount: The number of comments for the channel
-    - statistics.subscriberCount: The number of subscribers that the channel has
-    - statistics.videoCount: The number of videos uploaded to the channel
-    - brandingSettings.channel.keywords: Space-separated list of keywords associated w/ the channel
     """
     channel_detail = service.channels()
     # split string into list of channel ids
-    channel_ids = channel_ids.split(',')
+    channels_list = channel_ids.split(',')
     channels_path = 'data/channels/'
 
-    channels_in_dir = os.listdir(channels_path)
-    # remove the file extension for the files in the dir
-    channels_in_dir = [os.path.splitext(channel)[0]
-                       for channel in channels_in_dir]
+    # get list of channels for which there is no data in the channels_path dir
+    channels_list = check_channel_id(channels_list, channels_path)
 
-    to_remove = []
-    # do a lookup of the channel id in the /data/channels directory to see if a folder already exists
-    for channel in channel_ids:
-        if channel in channels_in_dir:
-            to_remove.append(channel)
-
-    # remove the channel id from the list
-    [channel_ids.remove(channel) for channel in to_remove]
-
-    if channel_ids:
-        # re-join channel ids into comma-separated string
-        channel_ids = ",".join(channel_ids)
+    if channels_list:
         # make the request to the resource
         request = channel_detail.list(
             part=part, id=channel_ids, maxResults=results
@@ -85,40 +114,69 @@ def get_channel_details(service, channel_ids, part='snippet,statistics,brandingS
 # TODO: - implement some sort of logging of the response
 #       - add exception handling
 def get_channel_videos(service, channel_id, part='snippet', type='video', results=50):
+    """
+    Get video data associated with a channel id and write the response to a JSON
+    file in data/videos.
+    """
     yt_search = service.search()
 
+    videos_path = 'data/videos/data/'
+    channels_path = 'data/channels/'
+    channel = check_channel_id([channel_id], videos_path)
+
     final_out = {}
-    # TODO: a call to the channel resource (part='statistics') will greatly reduce the need for
-    #       this sort of looping which is resource intensive on the API
-    #       because I will be able to see the video count from there and make a conditional
-    #       NOTE this can be done in ONE call by passing a string of comma-separated channel ids to the id parameter
-    for year in range(2000, datetime.today().year+1):
-        start = datetime(year, 1, 1)
-        end = start + relativedelta(years=1)
-        # prepare dates in a format the YT API is happy with
-        start, end = [date.isoformat('T')+'Z' for date in [start, end]]
+    if channel:
+        # how many videos the channel has
+        video_ct = get_response_item(
+            'statistics', 'videoCount', channel_id, channels_path
+        )
+        # the date the channel was created
+        year_pub = get_response_item(
+            'snippet', 'publishedAt', channel_id, channels_path
+        )
+        year_pub = datetime.strptime(year_pub, "%Y-%m-%dT%H:%M:%S%z").year
 
-        request = yt_search.list(part=part,
-                                 channelId=channel_id,
-                                 type=type,
-                                 publishedAfter=start,
-                                 publishedBefore=end,
-                                 maxResults=results
-                                 )
-        response = request.execute()
+        if int(video_ct) > 500:
+            for year in range(year_pub, datetime.today().year+1):
+                start = datetime(year, 1, 1)
+                end = start + relativedelta(years=1)
+                # prepare dates in a format the YT API is happy with
+                start, end = [date.isoformat('T')+'Z' for date in [start, end]]
 
-        # full response output for the year
-        data_out = page_through_response(yt_search, request, response)
+                request = yt_search.list(part=part,
+                                         channelId=channel_id,
+                                         type=type,
+                                         publishedAfter=start,
+                                         publishedBefore=end,
+                                         maxResults=results
+                                         )
+                response = request.execute()
 
-        # build on the items list in the final_out dict to form the complete response
-        if final_out:
-            final_out['items'].extend(data_out['items'])
+                # full response output for the year
+                data_out = page_through_response(yt_search, request, response)
+
+                # build on the items list in the final_out dict to form the complete response
+                if final_out:
+                    final_out['items'].extend(data_out['items'])
+                else:
+                    final_out = data_out
         else:
+            # TODO: refactor into some sort of helper func
+            request = yt_search.list(part=part,
+                                     channelId=channel_id,
+                                     type=type,
+                                     maxResults=results
+                                     )
+            response = request.execute()
+
+            # full response output for the year
+            data_out = page_through_response(yt_search, request, response)
+
             final_out = data_out
 
-    with open('out1.json', 'a+') as video_data:
-        response_obj = json.dumps(final_out)
-        video_data.write(response_obj)
+        with open(videos_path+'{}.json'.format(channel_id), 'w+') as video_data:
+            response_obj = json.dumps(final_out)
+            video_data.write(response_obj)
 
 
 def get_video_thumbnails(img_url):
@@ -134,16 +192,18 @@ def main():
     API_VERSION = 'v3'
 
     service = build(API_SERVICE_NAME, API_VERSION, developerKey=API_KEY)
-    channels = ['UCfzlCWGWYyIQ0aLC5w48gBQ',
-                'UCCUw_xsps3VgtqLYR8vBSXw',
-                'UCCezIgC97PvUuR4_gbFUs5g',
-                'UC_ML5xP23TOWKUcc-oAE_Eg']
+    channels = [
+        'UCfzlCWGWYyIQ0aLC5w48gBQ',
+        'UCCUw_xsps3VgtqLYR8vBSXw',
+        'UCCezIgC97PvUuR4_gbFUs5g',
+        'UC_ML5xP23TOWKUcc-oAE_Eg'
+    ]
 
     channels_str = ",".join(channels)
     get_channel_details(service, channels_str)
 
-    # for channel in channels:
-    #     get_channel_videos(service, channel)
+    for channel in channels:
+        get_channel_videos(service, channel)
 
 
 if __name__ == "__main__":
